483a40f6b72afd9481d07b1a1835a7cb821853c6
MateusC2 <mateuscubeiro.educ@gmail.com>
2025-03-26 12:43:44 -0300

    add defaultLayouts

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <Route
             path="/users"
             element={
-              <ProtectedRoute>
+              <ProtectedRoute >
                 <ListUsers/>
               </ProtectedRoute>
             }
